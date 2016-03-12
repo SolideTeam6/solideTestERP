aa72d1466cf508d95fe42e71634a0f4505b19538
= <=>
2016-03-12 13:58:24 -0700

    layout / plugins / landing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,28 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( js/demo.js )
+Rails.application.config.assets.precompile += %w( js/morris.js )
+Rails.application.config.assets.precompile += %w( js/jquery-jvectormap-world-mill-en.js )
+Rails.application.config.assets.precompile += %w( js/jQuery-2.1.4.min.js )
+Rails.application.config.assets.precompile += %w( js/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( js/app.js )
+Rails.application.config.assets.precompile += %w( js/app.min.js )
+Rails.application.config.assets.precompile += %w( js/pages/dashboard.js )
+Rails.application.config.assets.precompile += %w( js/jquery-jvectormap-1.2.2.min.js )
+Rails.application.config.assets.precompile += %w( js/jquery.sparkline.min.js )
+Rails.application.config.assets.precompile += %w( js/jquery.knob.js)
+Rails.application.config.assets.precompile += %w( js/daterangepicker.js )
+Rails.application.config.assets.precompile += %w( js/bootstrap-datepicker.js )
+Rails.application.config.assets.precompile += %w( js/fastclick.js )
+Rails.application.config.assets.precompile += %w( js/jquery.slimscroll.min.js )
+Rails.application.config.assets.precompile += %w( js/bootstrap3-wysihtml5.all.min.js )
+
+
+
+
+
+
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
